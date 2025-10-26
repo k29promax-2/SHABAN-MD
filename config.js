5e3b76a2705c50c3b99854c6afb3b811bdf2e3c0
k29promax-2 <daaaako24@gmail.com>
2025-10-26 18:15:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SMD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicUFySWVya09zZUlMLzFtMXd5dEc4bGc3R2d2NmZaR252UmwvNVpFNjVIMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicmNWUDZRM0VEY0xnaStzQ2VLQmJQUUZaWGRSbVRDdVltallPQmRDdUlFbz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJTQ1ZSZFB2aXUxVXJmbEs0Szl2bGptelVOV2FBbGlzQzlMYlRmTjRGRFhFPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIzMnFNdHkrTVdneTFpWFJhWkdmVGJwcjBjd2RVSmNaKytLY21iMkUvZkNNPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkFQLzl3Q2pjc1FoUFhXNkZWK3RZakF1U2JQQS9SbXVmSWUrd3VJK09Ca1k9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IllEK1pMVGpnYzlNeklGdXNNQXR6N2wySUlqS0YrWXlSdXA4U0V1VnRoWEk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUhtS3VSRFU0M1c5d2VNZStLcHdDeFlYREUzTFNnQWoyMDdtL0l3UjBuMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZisvWDA4Qmd4V3ZQNDVpWmdpUE5iVUlieEhhL2hXamg4WElDQUdaR0JHUT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InJnNmlSajhzSUh5ZGlUNnRLelNkUzkxK1JJakNZSTlnMXUxU1owdXBIa1hTYVBXaG5zOERiTzdvRkMrQlEwTm16dEovZUpRTnVRRzVtbWpLMGNMTmhBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTY5LCJhZHZTZWNyZXRLZXkiOiJ4TTVSckdWMm9Sb2NiWXM3UnR4SmFUS2dTdmxCT0xYSmpHQ1gya2J3YlMwPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwicmVnaXN0ZXJlZCI6dHJ1ZSwicGFpcmluZ0NvZGUiOiJHTjhESFdWNiIsIm1lIjp7ImlkIjoiMjYzNzg3ODEyNTI5OjlAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIzNjA4MjUwNjg0MjEzMzo5QGxpZCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDT0hUNG1rUWdkSDJ4d1lZQVNBQUtBQT0iLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiSE1LemRJeGFXV1JFZk5uMC85SEU3OVJJYUp0Rms2ZUg5TEFuYlAwRnQzWT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiZ0RHWTdXdWtsYjhxSmhtWVhFeXV0Y2s3SSthby9KRURCT24rL05oYmt0bHZEUUlPNFRTang4Q2JkRU9TQTR0Z3B1UkVsZHUyaDVZZHEvQXdoRmlUQVE9PSIsImRldmljZVNpZ25hdHVyZSI6InFNSWZFZXdXbkZ5NWFuMFUyeTlFN05DcmpRRkN4c3dDa1owTitMdHRNNkRYM3E3KzZTd2FnU0g0c3lxUFkyQ2lvckZZOW5CK0N3Z0RSRFkvMlF4OWdBPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjYzNzg3ODEyNTI5OjlAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCUnpDczNTTVdsbGtSSHpaOVAvUnhPL1VTR2liUlpPbmgvU3dKMno5QmJkMiJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FVSURRPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzYxNDU0MjEzLCJsYXN0UHJvcEhhc2giOiIyUDFZaGYifQ==",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -33,11 +33,11 @@ CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself  
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923303788282",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "263787382852",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "Imran-Maani",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Sʜᴀʙᴀɴ*",
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ k29promax*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
 // add img for alive msg 
